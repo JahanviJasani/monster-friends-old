6d2d3515f4f52d9917a808c7b60bf7372838d86e
JahanviJasani <jasanijahanvi@gmail.com>
2021-04-05 19:52:05 +0530

    Created the search box component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './App.css';
+import SearchBox from './SearchBox'
 import CardList from './CardList'
 import { monsters } from './monsters'
 
 const App = () => {
   return (
     <div className="tc">
+      <h1 className="code">Monster Friends</h1>
+      <SearchBox />
       <CardList monsters={monsters}/>
     </div>
   )
---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <div>
+      <input
+        className ="pa3 mb3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="Search Monsters"
+      />
+    </div>
+  )
+}
+
+export default SearchBox
